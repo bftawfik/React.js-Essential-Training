7474b885e555cca9513d44c3566ff2c0b9adc9f2
Bishoy Farouk Tawfik <bftawfik@gmail.com>
2018-12-24 13:25:57 +0200

    Adding the needed modules for react

---
+++ my-project/webpack.config.js
@@ -0,0 +1,31 @@
+// for more help with webpack look at https://www.robinwieruch.de/minimal-react-webpack-babel-setup/
+
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
