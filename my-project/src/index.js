37b0be3a8ec74746cd088f2aae46f133eff92907
Bishoy Farouk Tawfik <bftawfik@gmail.com>
2018-12-25 12:00:49 +0200

    fixing the webpack configuration to be working with react router

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 
 import './stylesheets/ui.scss'
 import { App } from './components/App'
+import { Whoops404 } from './components/Whoops404'
+
+import { Switch, BrowserRouter, Route, Link, hashHistory } from "react-router-dom";
+
 
 window.React = React;
 
 ReactDOM.render(
-  <App/>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App}/>
+      <Route exact path="*" component={Whoops404}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('app')
 );
 module.hot.accept();
--- my-project/webpack.config.js
+++ my-project/webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
   ],
   devServer: {
     contentBase: './dist',
+    historyApiFallback: true,
     hot: true
   }
 };
